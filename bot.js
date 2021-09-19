6bacb165901b7a527772b8fbbddc4d5ff4d006a6
ToxicBananaParty <ryan@rydwy.com>
2021-09-18 20:45:27 -0700

    Comment out code

--- bot.js
+++ bot.js
@@ -1,3 +1,6 @@
+// TODO:
+// - Prevent duplicate links (parsed.json?)
+
 const fs = require('fs');
 const twit = require('twit');
 const twilio = require('twilio');
@@ -5,6 +8,7 @@ const twilio = require('twilio');
 /*=============
     STARTUP
 ==============*/
+// Init twilio and twitter bots
 const senderNum = '+16179345848'
 const phoneNums = ['+15138822228', '+17657601991'];
 const texter = new twilio('ACb2c781c92ebc857aeb4717abb1964ffa', 'b0487e88dcbb4535fc5a54e08211324f');
@@ -17,16 +21,19 @@ const twitter = new twit({
     strictSSL: false
 });
 
+// Get UserIDs from handles.json
 const userIdPairs = JSON.parse(fs.readFileSync('./handles.json', 'utf-8'));
 let users = [];
 for(let username in userIdPairs) {
     users.push(userIdPairs[username]);
 }
 
+// Start twitter stream
 let stream = twitter.stream('statuses/filter', {
     follow: users
 });
 
+// Catch tweets from stream
 stream.on('tweet', (tweet) => {
     if(tweet.entities.urls) {
         console.log("INSIDE SPECIAL IF\nURLS ARR IS: " + tweet.entities.urls[0]);
@@ -41,6 +48,7 @@ stream.on('tweet', (tweet) => {
 /*=====================================
     PARSE TWEET & PREPARE RESPONSE
 ======================================*/
+// Figure out if tweet is linking tickets for sale
 function parseTweet(tweet) {
     console.log(tweet);
 }
